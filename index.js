64ea7eb00b80043395272b54c908a3a7fe10b602
shojib deb <shodeb123@gmail.com>
2023-01-20 13:22:32 +0000

    move check in a different module

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var prompt = require('prompt-sync')();
+const check = require('./wonCheck');
 
 function ticTacToe() {
-
-
-    console.log("Welcome to the exciting game of Tic Tac Toe! This is a battle to decide the fate of the world. The name of the game is to be the first person to dominate the battlefield with three in a row! The first player is represented by the O and the second player the X.")
-
-    ticTacToeMove();
 
     var howManyPlayers = Number(prompt("How many people would like to play? (1 or 2): "))
     if (howManyPlayers == 1) {
@@ -16,21 +12,18 @@ function ticTacToe() {
         ticTacToeMove();
     }
     else(console.log("Invalid input, please try again."))
-
 }
 
 
 // Create board for game
 var board = ["_", "_", "_",
-    "_", "_", "_",
-    "_", "_", "_"];
-
+             "_", "_", "_",
+             "_", "_", "_"];
 
 // variables to keep track of current player
 
 
-var playerTurn = true;
-
+let playerTurn = true;
 
 // create function to display board 
 function displayBoard() {
@@ -43,34 +36,13 @@ function displayBoard() {
 
 displayBoard();
 
-
 // create function to check for who's won
 
-
-function wonCheck(board, playerTurn) {
-
-    const a = [0, 3, 6, 0, 1, 2, 0, 2];
-    const b = [1, 4, 8, 3, 4, 5, 4, 4];
-    const c = [2, 5, 8, 6, 7, 8, 8, 6];
-
-    a.forEach((num1, index) => {
-        const num2 = b[index]
-        const num3 = c[index]
-        console.log(num1, num2, num3)
-
-        if (new Set([board[num1], board[num2], board[num3]]).size == 1 && playerTurn == true) {
-            return 0
-        } else if (new Set([board[num1], board[num2], board[num3]]).size == 1 && playerTurn == false) {
-            return 1
-        }
-    })
-
-}
 
 
 function robotGame() {
     console.log("Foolish human you dare challenge the robot overlord, how foolish. Just to give you a chance I will let you make the first move!")
-    
+
     var playerMove = Number(prompt("Enter a number below 10: "))
     let robotChoice = ""
 
@@ -140,119 +112,104 @@ function robotGame() {
             break;
         default: console.log("Invalid move, please try again.");
     }
-
+    displayBoard();
 }
-
-	displayBoard();
 
 function ticTacToeMove() {
 
-    while (playerTurn <= 9) {
-        var playerMove = Number(prompt("Enter a number below 10: "))
-        switch (true) {
-            case playerMove == 1 && board[0] == "_":
-                if (playerTurn == true) {
-                    board[0] = "O";
-                    playerTurn = false;
-                    console.log("we went in!")
-                } else {
-                    board[0] = "X";
-                    playerTurn = true;
-                }
-                break;
-            case playerMove == 2 && board[1] == "_":
-                if (playerTurn == true) {
-                    board[1] = "O";
-                    playerTurn = false;
-                    console.log("we went in!")
-                } else {
-                    board[1] = "X";
-                    playerTurn = true;
-                }
-                break;
-            case playerMove == 3 && board[2] == "_":
-                if (playerTurn == true) {
-                    board[2] = "O";
-                    playerTurn = false;
-                } else {
-                    board[2] = "X";
-                    playerTurn = true;
-                }
-                break;
-            case playerMove == 4 && board[3] == "_":
-                if (playerTurn == true) {
-                    board[3] = "O";
-                    playerTurn = false;
-                } else {
-                    board[3] = "X";
-                    playerTurn = true;
-                }
-                break;
-            case playerMove == 5 && board[4] == "_":
-                if (playerTurn == true) {
-                    board[4] = "O";
-                    playerTurn = false;
-                } else {
-                    board[4] = "X";
-                    playerTurn = true;
-                }
-                break;
-            case playerMove == 6 && board[5] == "_":
-                if (playerTurn == true) {
-                    board[5] = "O";
-                    playerTurn = false;
-                } else {
-                    board[5] = "X";
-                    playerTurn = true;
-                }
-                break;
-            case playerMove == 7 && board[6] == "_":
-                if (playerTurn == true) {
-                    board[6] = "O";
-                    playerTurn = false;
-                } else {
-                    board[6] = "X";
-                    playerTurn = true;
-                }
-                break;
-            case playerMove == 8 && board[7] == "_":
-                if (playerTurn == true) {
-                    board[7] = "O";
-                    playerTurn = false;
-                } else {
-                    board[7] = "X";
-                    playerTurn = true;
-                }
-                break;
-            case playerMove == 9 && board[8] == "_":
-                if (playerTurn == true) {
-                    board[8] = "O";
-                    playerTurn = false;
-                } else {
-                    board[8] = "X";
-                    playerTurn = true;
-                }
-                break;
-            default: console.log("Invalid move, please try again.");
-        }
+    var playerMove = Number(prompt("Enter a number from 1 to 9: "))
+
+    switch (true) {
+        case playerMove == 1 && board[0] == "_":
+            if (playerTurn == true) {
+                board[0] = "O";
+                playerTurn = false;
+                console.log("we went in!")
+            } else {
+                board[0] = "X";
+                playerTurn = true;
+            }
+            break;
+        case playerMove == 2 && board[1] == "_":
+            if (playerTurn == true) {
+                board[1] = "O";
+                playerTurn = false;
+                console.log("we went in!")
+            } else {
+                board[1] = "X";
+                playerTurn = true;
+            }
+            break;
+        case playerMove == 3 && board[2] == "_":
+            if (playerTurn == true) {
+                board[2] = "O";
+                playerTurn = false;
+            } else {
+                board[2] = "X";
+                playerTurn = true;
+            }
+            break;
+        case playerMove == 4 && board[3] == "_":
+            if (playerTurn == true) {
+                board[3] = "O";
+                playerTurn = false;
+            } else {
+                board[3] = "X";
+                playerTurn = true;
+            }
+            break;
+        case playerMove == 5 && board[4] == "_":
+            if (playerTurn == true) {
+                board[4] = "O";
+                playerTurn = false;
+            } else {
+                board[4] = "X";
+                playerTurn = true;
+            }
+            break;
+        case playerMove == 6 && board[5] == "_":
+            if (playerTurn == true) {
+                board[5] = "O";
+                playerTurn = false;
+            } else {
+                board[5] = "X";
+                playerTurn = true;
+            }
+            break;
+        case playerMove == 7 && board[6] == "_":
+            if (playerTurn == true) {
+                board[6] = "O";
+                playerTurn = false;
+            } else {
+                board[6] = "X";
+                playerTurn = true;
+            }
+            break;
+        case playerMove == 8 && board[7] == "_":
+            if (playerTurn == true) {
+                board[7] = "O";
+                playerTurn = false;
+            } else {
+                board[7] = "X";
+                playerTurn = true;
+            }
+            break;
+        case playerMove == 9 && board[8] == "_":
+            if (playerTurn == true) {
+                board[8] = "O";
+                playerTurn = false;
+            } else {
+                board[8] = "X";
+                playerTurn = true;
+            }
+            break;
+        default: console.log("Invalid move, please try again.");
     }
+	displayBoard();
+
 }
 
 // ask player for current move (location on board)
-function playAnother() {
-    var playAgain = prompt("Would you like to play again? (Y/N): ").toLowerCase();
-    if (playAgain == "y") {
-        board = ["_", "_", "_",
-        "_", "_", "_",
-        "_", "_", "_"];
-        playerTurn = true;
-        ticTacToe();
-    } else if (playAgain == "n") {
-        console.log("Thank you for playing!")
-    } else {
-        console.log("Invalid input, please try again.")
-    }
-}
 
 ticTacToe();
 
