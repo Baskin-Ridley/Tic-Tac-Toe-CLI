afa4ba420b9792b7a65248bfe17f044a4d252ee8
“sadik312” <sadikislam46@gmail.com>
2023-01-20 11:01:21 +0000

    created function to display board

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // Create board for game
-let board = ["_", "_", "_",
-             "_", "_", "_",
-             "_", "_", "_"];
+let board = [" ", " ", " ",
+             " ", " ", " ",
+             " ", " ", " "];
 
 
 // variables to keep track of current player
 
 // create function to display board 
+function displayBoard() {
+	console.log(board[0] + " | " + board[1] + " | " + board[2]);
+    console.log("---------");
+	console.log(board[3] + " | " + board[4] + " | " + board[5]);
+    console.log("---------");
+	console.log(board[6] + " | " + board[7] + " | " + board[8]);
+}
+
+displayBoard();
 
 
 // create function to check for who's won
